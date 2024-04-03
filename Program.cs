ad09c1e11532d271f189b198eb1c6ef22403f9ca
Raulnss <raulnsantosdesousa@gmail.com>
2024-04-03 19:55:07 -0300

    C#

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teste2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" numero: ");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" sinal da operação: ");
+           string op = Console.ReadLine();
+            Console.WriteLine(" numero: ");
+            int n2 = int.Parse(Console.ReadLine());
+            if (op == "+")
+            {
+                Console.WriteLine(n1 + n2); 
+            }
+            else if (op == "-") {
+                Console.WriteLine(n1 - n2);
+            }
+            else if (op == "*" || op == "x")
+            {
+                Console.WriteLine(n1 * n2);
+            } 
+            else if (op == "/")
+            {
+                Console.WriteLine(n1 / n2);
+            }
+            else if(op == "%")
+            {
+                Console.WriteLine(n1 % n2);
+            }
+            else
+            {
+                Console.WriteLine(" algo deu errado! ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
